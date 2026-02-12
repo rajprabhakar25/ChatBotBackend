71967826b1431078487ba0f31b4a1e55ca0d68a2
Raj Prabhakar <rajprabhakar3107@gmail.com>
2026-02-12 19:21:16 +0530

    cors updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://chatbot-beta-sepia-94.vercel.app/',
+      'https://chatbot-beta-sepia-94.vercel.app',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
